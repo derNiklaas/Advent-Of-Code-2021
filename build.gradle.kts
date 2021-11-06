123369b2966511aa99514966c91b38f39c172754
Niklas <derNiklaas@users.noreply.github.com>
2021-11-06 02:50:32 +0100

    Update to JVM version 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     kotlin("jvm") version "1.6.0-M1"
 }
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31")
     testImplementation(platform("org.junit:junit-bom:5.8.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 repositories {
     mavenCentral()
@@ -21,9 +21,9 @@ tasks.test {
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "16"
+    jvmTarget = "17"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "16"
+    jvmTarget = "17"
 }
