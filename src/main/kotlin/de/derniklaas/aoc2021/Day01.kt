3e76cc686eea40324ffa266767b9c5dfea6e137e
Niklas <derNiklaas@users.noreply.github.com>
2021-12-01 08:43:17 +0100

    Add Day01 Code

--- src/main/kotlin/de/derniklaas/aoc2021/Day01.kt
+++ src/main/kotlin/de/derniklaas/aoc2021/Day01.kt
@@ -1,5 +1,35 @@
 package de.derniklaas.aoc2021
 
-public fun main() {
+import java.io.File
 
+public fun main() {
+    val input = File("src/main/resources/Day01.txt").readLines().mapToInt()
+    val day01 = Day01(input)
+    println("Part 1: ${day01.part1()}")
+    println("Part 2: ${day01.part2()}")
+}
+
+public class Day01(private val input: List<Int>) {
+
+    public fun part1(): Int {
+        var previous = input[0]
+        val times = input.subList(1, input.size).count {
+            val result = it > previous
+            previous = it
+            result
+        }
+        return times
+    }
+
+    public fun part2(): Int {
+        var times = 0
+        for (i in 3 until input.size) {
+            val sum = input[i] + input[i - 1] + input[i - 2]
+            val previousSum = input[i - 1] + input[i - 2] + input[i - 3]
+            if (sum > previousSum) {
+                times++
+            }
+        }
+        return times
+    }
 }
--- src/test/kotlin/de/derniklaas/aoc2021/Day01Tests.kt
+++ src/test/kotlin/de/derniklaas/aoc2021/Day01Tests.kt
@@ -1,5 +1,21 @@
 package de.derniklaas.aoc2021
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class Day01Tests {
 
+    @Test
+    public fun part1() {
+        val inputs = listOf("199", "200", "208", "210", "200", "207", "240", "269", "260", "263").mapToInt()
+        val day01 = Day01(inputs)
+        Assertions.assertEquals(7, day01.part1())
+    }
+
+    @Test
+    public fun part2() {
+        val inputs = listOf("199", "200", "208", "210", "200", "207", "240", "269", "260", "263").mapToInt()
+        val day01 = Day01(inputs)
+        Assertions.assertEquals(5, day01.part2())
+    }
 }
