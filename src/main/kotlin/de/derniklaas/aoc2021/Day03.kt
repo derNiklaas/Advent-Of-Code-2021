6a985c1d07d7a0e9daf1d08787c60cf8799815ae
Niklas <derNiklaas@users.noreply.github.com>
2021-12-03 06:57:33 +0100

    Day 3

---
+++ src/main/kotlin/de/derniklaas/aoc2021/Day03.kt
@@ -0,0 +1,72 @@
+package de.derniklaas.aoc2021
+
+import java.io.File
+
+fun main() {
+    val input = File("src/main/resources/Day03.txt").readLines()
+    val day03 = Day03(input)
+    println("Part 1: ${day03.part1()}")
+    println("Part 2: ${day03.part2()}")
+}
+
+public class Day03(private val input: List<String>) {
+
+    public fun part1(): Int {
+        val amount = input[0].length
+        val map = generateMap(input)
+
+        var mostCommonString = ""
+        var leastCommonString = ""
+
+        for (i in 0 until amount) {
+            if (map[i]?.toDouble()!! / input.size >= 0.5) {
+                mostCommonString += "1"
+                leastCommonString += "0"
+            } else {
+                mostCommonString += "0"
+                leastCommonString += "1"
+            }
+        }
+        println("$leastCommonString $mostCommonString")
+        return leastCommonString.toInt(2) * mostCommonString.toInt(2)
+    }
+
+    public fun part2(): Int {
+        var possibleOxygenRatings = input
+        var possibleCO2Ratings = input
+        for (i in 0 until input[0].length) {
+            if (possibleOxygenRatings.size != 1) {
+                possibleOxygenRatings = filter(possibleOxygenRatings, i, true)
+            }
+            if (possibleCO2Ratings.size != 1) {
+                possibleCO2Ratings = filter(possibleCO2Ratings, i, false)
+            }
+        }
+
+        println("${possibleOxygenRatings[0]} ${possibleCO2Ratings[0]}")
+
+        return possibleOxygenRatings[0].toInt(2) * possibleCO2Ratings[0].toInt(2)
+    }
+
+    private fun filter(input: List<String>, position: Int, oxygen: Boolean = true): List<String> {
+        val map = generateMap(input)
+        val needed = if (map[position]!!.toDouble() / input.size >= 0.5) {
+            if (oxygen) '1' else '0'
+        } else {
+            if (oxygen) '0' else '1'
+        }
+        return input.filter { it[position] == needed }
+    }
+
+    private fun generateMap(input: List<String>): MutableMap<Int, Int> {
+        val map = mutableMapOf<Int, Int>()
+        input.forEach { s ->
+            s.split("").forEachIndexed { index, s2 ->
+                if (s2 == "1") {
+                    map[index - 1] = map[index - 1]?.plus(1) ?: 1
+                }
+            }
+        }
+        return map
+    }
+}
---
+++ src/test/kotlin/de/derniklaas/aoc2021/Day03Tests.kt
@@ -0,0 +1,47 @@
+package de.derniklaas.aoc2021
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day03Tests {
+
+    @Test
+    public fun part1() {
+        val inputs = listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        )
+        val day03 = Day03(inputs)
+        Assertions.assertEquals(198, day03.part1())
+    }
+
+    @Test
+    public fun part2() {
+        val inputs = listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        )
+        val day03 = Day03(inputs)
+        Assertions.assertEquals(230, day03.part2())
+    }
+}
