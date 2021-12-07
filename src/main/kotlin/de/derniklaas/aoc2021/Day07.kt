02a5fce3cfb378ad23dccf359c88e4bceec867f6
Niklas <derNiklaas@users.noreply.github.com>
2021-12-07 14:29:57 +0100

    I think a sorted list is also a great option

--- src/main/kotlin/de/derniklaas/aoc2021/Day07.kt
+++ src/main/kotlin/de/derniklaas/aoc2021/Day07.kt
@@ -6,7 +6,7 @@ import kotlin.math.abs
 // o7 in the chat
 
 fun main() {
-    val input = File("src/main/resources/Day07.txt").readLines().first().splitAndMapToInt(",")
+    val input = File("src/main/resources/Day07.txt").readLines().first().splitAndMapToInt(",").sorted()
     val day07 = Day07(input)
     println("Part 1: ${day07.part1()}")
     println("Part 2: ${day07.part2()}")
@@ -17,26 +17,16 @@ public class Day07(private val input: List<Int>) {
 
     public fun part2() = calculateCost(true)
 
-    private fun calculateCost(secondPart: Boolean = false): Int {
-        var smallestCost = Int.MAX_VALUE
-        val smallestPos = input.minOf { it }
-        val largestPos = input.maxOf { it }
+    private fun calculateCost(secondPart: Boolean = false) =
+        input.first().rangeTo(input.last()).minOf { alignedPosition ->
+            input.sumOf {
+                if (secondPart) {
 
-        for (alignedPosition in smallestPos..largestPos) {
-            val cost = input.sumOf {
-                val sum = if (secondPart) {
                     val steps = abs(it - alignedPosition)
                     (steps * steps + steps) / 2
                 } else {
                     abs(it - alignedPosition)
                 }
-
-                sum
             }
-            if (cost < smallestCost) smallestCost = cost
         }
-
-        return smallestCost
-    }
-
 }
