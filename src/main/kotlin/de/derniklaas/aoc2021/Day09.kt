1d7cf413bd4048e6dc9c1d460e328e8531b092b7
Niklas <derNiklaas@users.noreply.github.com>
2021-12-09 15:15:21 +0100

    Small Day 9 refactoring

--- src/main/kotlin/de/derniklaas/aoc2021/Day09.kt
+++ src/main/kotlin/de/derniklaas/aoc2021/Day09.kt
@@ -18,35 +18,23 @@ public class Day09(private val input: List<List<Int>>) {
 
         for (i in input.indices) {
             for (j in input[0].indices) {
-                var lowestPoint = true
-
                 // The biggest one-digit number is 9, so there can only be smaller numbers or 9 around it.
                 if (input[i][j] == 9) continue
 
                 if (i - 1 in input.indices) { // Up
-                    if (input[i - 1][j] <= input[i][j]) {
-                        lowestPoint = false
-                    }
+                    if (input[i - 1][j] <= input[i][j]) continue
                 }
                 if (i + 1 in input.indices) { // Down
-                    if (input[i + 1][j] <= input[i][j]) {
-                        lowestPoint = false
-                    }
+                    if (input[i + 1][j] <= input[i][j]) continue
                 }
                 if (j - 1 in input[0].indices) { // Left
-                    if (input[i][j - 1] <= input[i][j]) {
-                        lowestPoint = false
-                    }
+                    if (input[i][j - 1] <= input[i][j]) continue
                 }
                 if (j + 1 in input[0].indices) { // Right
-                    if (input[i][j + 1] <= input[i][j]) {
-                        lowestPoint = false
-                    }
+                    if (input[i][j + 1] <= input[i][j]) continue
                 }
-                if (lowestPoint) {
-                    points += input[i][j] + 1
-                    basins += Pair(i, j)
-                }
+                points += input[i][j] + 1
+                basins += Pair(i, j)
             }
         }
 
@@ -65,10 +53,14 @@ public class Day09(private val input: List<List<Int>>) {
     }
 
     private fun getSize(i: Int, j: Int): Int {
+        // Check if [i] and [j] are in the grid
+        if (i !in input.indices || j !in input[0].indices) return 0
+
+        // Check if the point is a 9 (the highest number)
+        if (input[i][j] == 9) return 0
+
         // Ignore already visited points
-        if (visited[i][j]) {
-            return 0
-        }
+        if (visited[i][j]) return 0
 
         // Mark point as visited
         visited[i][j] = true
@@ -76,26 +68,10 @@ public class Day09(private val input: List<List<Int>>) {
         var sum = 1
 
         // Check all directions
-        if (i - 1 in input.indices) { // Up
-            if (input[i - 1][j] != 9) {
-                sum += getSize(i - 1, j)
-            }
-        }
-        if (i + 1 in input.indices) { // Down
-            if (input[i + 1][j] != 9) {
-                sum += getSize(i + 1, j)
-            }
-        }
-        if (j - 1 in input[0].indices) { // Left
-            if (input[i][j - 1] != 9) {
-                sum += getSize(i, j - 1)
-            }
-        }
-        if (j + 1 in input[0].indices) { // Right
-            if (input[i][j + 1] != 9) {
-                sum += getSize(i, j + 1)
-            }
-        }
+        sum += getSize(i - 1, j)
+        sum += getSize(i + 1, j)
+        sum += getSize(i, j - 1)
+        sum += getSize(i, j + 1)
 
         return sum
     }
