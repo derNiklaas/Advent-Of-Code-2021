d8f1dc841b95b46a541869c18585455e83fa7f38
Niklas <derNiklaas@users.noreply.github.com>
2021-12-23 15:35:19 +0100

    Upload Day 23 results :D

---
+++ src/main/kotlin/de/derniklaas/aoc2021/Day23.kt
@@ -0,0 +1,27 @@
+package de.derniklaas.aoc2021
+
+public fun main() {
+    val day23 = Day23()
+
+    println("Yep, I totally used really fancy Kotlin Code to solve this.")
+    println("So please just use this website: https://amphipod.net/")
+    println("Have fun! :)")
+    println()
+
+    println("Part 1: ${day23.part1()}")
+    println("Part 2: ${day23.part2()}")
+}
+
+/*
+I might consider actually coding a solution at some point, but that point is not today. :)
+*/
+
+public class Day23 {
+    public fun part1(): Int {
+        return 13495
+    }
+
+    public fun part2(): Int {
+        return 53767
+    }
+}
