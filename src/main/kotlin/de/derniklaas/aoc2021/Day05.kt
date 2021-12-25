cf151171c0c8fe8c791c795a64483f705a3c6835
Niklas <derNiklaas@users.noreply.github.com>
2021-12-25 21:21:27 +0100

    Cleanup

--- src/main/kotlin/de/derniklaas/aoc2021/Day05.kt
+++ src/main/kotlin/de/derniklaas/aoc2021/Day05.kt
@@ -3,52 +3,45 @@ package de.derniklaas.aoc2021
 import java.io.File
 
 public fun main() {
-    val input = File("src/main/resources/Day05.txt").readLines().map { Line.toLine(it) }
+    val input = File("src/main/resources/Day05.txt").readLines()
     val day05 = Day05(input)
     println("Part 1: ${day05.part1()}")
     println("Part 2: ${day05.part2()}")
 }
 
-public class Day05(private val input: List<Line>) {
-
+public class Day05(input: List<String>) {
+    private val lines = input.map { toLine(it) }
     private val grid: Array<Array<Int>>
 
     init {
-        val maxX = input.maxOf { if (it.start.x > it.end.x) it.start.x else it.end.x }
-        val maxY = input.maxOf { if (it.start.y > it.end.y) it.start.y else it.end.y }
+        val maxX = lines.maxOf { it.maxX }
+        val maxY = lines.maxOf { it.maxY }
 
         grid = Array(maxY + 1) { Array(maxX + 1) { 0 } }
     }
 
     public fun part1(): Int {
-        for (line in input) {
+        for (line in lines) {
             // Only consider horizontal and vertical lines
             if (!(line.start.x == line.end.x || line.start.y == line.end.y)) continue
 
-            val maxX = if (line.start.x > line.end.x) line.start.x else line.end.x
-            val maxY = if (line.start.y > line.end.y) line.start.y else line.end.y
-            val minX = if (line.start.x < line.end.x) line.start.x else line.end.x
-            val minY = if (line.start.y < line.end.y) line.start.y else line.end.y
-
-            for (x in minX..maxX) {
-                for (y in minY..maxY) {
+            for (x in line.minX..line.maxX) {
+                for (y in line.minY..line.maxY) {
                     grid[y][x]++
                 }
             }
         }
 
         return grid.sumOf { it.count { number -> number >= 2 } }
-
     }
 
     public fun part2(): Int {
-
-        for (line in input) {
+        for (line in lines) {
             // Only calculate diagonal lines, horizontal and vertical lines have already been counted in part1
             if (line.start.x == line.end.x || line.start.y == line.end.y) continue
 
-            val maxX = if (line.start.x > line.end.x) line.start.x else line.end.x
-            val minX = if (line.start.x < line.end.x) line.start.x else line.end.x
+            val maxX = line.maxX
+            val minX = line.minX
             val directionX = if (line.start.x > line.end.x) -1 else 1
             val directionY = if (line.start.y > line.end.y) -1 else 1
 
@@ -61,16 +54,22 @@ public class Day05(private val input: List<Line>) {
 
         return grid.sumOf { it.count { number -> number >= 2 } }
     }
+
+    /**
+     *  Turns a String from the input into a line
+     *  Example: 0,9 -> 5,9 turns into a line from (0,9) to (5,9)
+     */
+    private fun toLine(input: String): Line {
+        val (a, b) = input.split(" -> ")
+        val (a1, a2) = a.splitAndMapToInt(",")
+        val (b1, b2) = b.splitAndMapToInt(",")
+        return Line(Point(a1, a2), Point(b1, b2))
+    }
 }
 
-public data class Line(public val start: Point, public val end: Point) {
-    public companion object {
-        // Example: 0,9 -> 5,9
-        public fun toLine(input: String): Line {
-            val (a, b) = input.split(" -> ")
-            val (a1, a2) = a.splitAndMapToInt(",")
-            val (b1, b2) = b.splitAndMapToInt(",")
-            return Line(Point(a1, a2), Point(b1, b2))
-        }
-    }
+private data class Line(public val start: Point, public val end: Point) {
+    public val minX = minOf(start.x, end.x)
+    public val maxX = maxOf(start.x, end.x)
+    public val minY = minOf(start.y, end.y)
+    public val maxY = maxOf(start.y, end.y)
 }
--- src/test/kotlin/de/derniklaas/aoc2021/Day05Tests.kt
+++ src/test/kotlin/de/derniklaas/aoc2021/Day05Tests.kt
@@ -7,14 +7,13 @@ import org.junit.jupiter.api.Test
 public class Day05Tests {
     @Test
     public fun part1() {
-        val inputs = File("src/test/resources/Day05.txt").readLines().map { Line.toLine(it) }
-        val day05 = Day05(inputs)
-        Assertions.assertEquals(5, day05.part1())
+        val inputs = File("src/test/resources/Day05.txt").readLines()
+        Assertions.assertEquals(5, Day05(inputs).part1())
     }
 
     @Test
     public fun part2() {
-        val inputs = File("src/test/resources/Day05.txt").readLines().map { Line.toLine(it) }
+        val inputs = File("src/test/resources/Day05.txt").readLines()
         val day05 = Day05(inputs)
         day05.part1()
         Assertions.assertEquals(12, day05.part2())
